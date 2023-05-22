c060fefec028ea976861327bb1fe8d5b7f459ff2
bubaleh1337 <ekaterina.pyshkova@gmail.com>
2023-05-22 22:28:06 +0500

    added task2 and solution, of hw7 sem7

--- seminars/sem7/hw7/task2.py
+++ seminars/sem7/hw7/task2.py
@@ -21,11 +21,10 @@
 6 12 18 24 30 36
 """
 
-def print_operation_table(operation, num_rows=6, num_columns=6):
+def print_operation_table(operation, num_rows=6, num_columns=6):    
     for row in range(1, num_rows + 1):
         for column in range(1, num_columns + 1):
-            result = operation(row, column)
-            print(result, end=" ")
+            print(operation(row, column), end="\t")
         print()
 
 print_operation_table(lambda x, y: x * y)
