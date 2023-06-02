b2534b2f16679c5db6bff8aa3fe9944921b11ac2
bubaleh1337 <ekaterina.pyshkova@gmail.com>
2023-06-02 18:03:09 +0500

    was added class Phonebook in controller.py and model.py

--- seminars/sem9/hw9/controller.py
+++ seminars/sem9/hw9/controller.py
@@ -2,37 +2,38 @@ import text
 import view
 import model
 
+my_pb = model.PhoneBook()
 def start():
 		while True:
 				choice = view.main_menu() # вывод меню и предоставление выбора
 				match choice:
 						case 1: # Открыть файл
-								model.open_pb()
+								my_pb.open_pb()
 								view.print_message(text.load_successful)
 						case 2: # Записать файл
-								model.save_pb()
+								my_pb.save_pb()
 								view.print_message(text.save_successful)
 						case 3: # Показать контакт
-								pb = model.get_pb()
+								pb = my_pb.get_pb()
 								view.print_contacts(pb, text.load_error)
 						case 4: # Добавить контакт 
 								contact = view.input_contact(text.input_new_contact, text.cancel_input)
-								name = model.add_contact(contact)
+								name = my_pb.add_contact(contact)
 								view.print_message(text.new_contact_successful(name)) # type: ignore
 						case 5: # Найти контакт
 								word = view.input_search(text.input_search)
-								result = model.search_pb(word)
+								result = my_pb.search_pb(word)
 								view.print_contacts(result, text.empty_search(word))
 						case 6: # Изменить контакт
-								pb = model.get_pb()
+								pb = my_pb.get_pb()
 								index = view.input_index(text.input_index, pb, text.load_error)
 								contact = view.input_contact(text.input_new_contact, text.cancel_input)
-								result = model.change_pb(contact, index)
+								result = my_pb.change_pb(contact, index)
 								view.print_message(text.change_successful(contact))
 						case 7: # Удалить контакт 
-								pb = model.get_pb()
+								pb = my_pb.get_pb()
 								index = view.input_index(text.index_del_contact, pb, text.load_error)
-								name = model.del_contact(index)
+								name = my_pb.del_contact(index)
 								view.print_message(text.del_contact(name)) # type: ignore
 						case 8:
 								break
--- seminars/sem9/hw9/model.py
+++ seminars/sem9/hw9/model.py
@@ -1,55 +1,52 @@
 from contextlib import suppress
 
-phone_book: list[dict[str, str]] = []
-path = 'phones.txt'
+class PhoneBook:
 
-def open_pb():
-	global phone_book, path
-	with open(path, 'r', encoding='UTF-8') as file:
-		data = file.readlines() # 'Kate:7444654:comment'
-	for contact in data:
-		contact = contact.strip().split(':') # .strip() - очищение начало и конец строки
-		phone_book.append({'name': contact[0], 'phone': contact[1], 'comment': contact[2]})
+	def __init__(self, path: str = 'phones.txt'):
+		self._phone_book: list[dict[str, str]] = []
+		self._path = path
 
-def save_pb():
-	global phone_book, path
-	data = []
-	for contact in phone_book:
-		data.append(':'.join([value for value in contact.values()]))
-	with open(path, 'w', encoding='UTF-8') as file:
-		file.write('\n'.join(data))
+	def open_pb(self):
+		with open(self._path, 'r', encoding='UTF-8') as file:
+			data = file.readlines() # 'Kate:7444654:comment'
+		for contact in data:
+			contact = contact.strip().split(':') # .strip() - очищение начало и конец строки
+			self._phone_book.append({'name': contact[0], 'phone': contact[1], 'comment': contact[2]})
 
-def get_pb() -> list[dict[str, str]]:
-	global phone_book
-	return phone_book
+	def save_pb(self):
+		data = []
+		for contact in self._phone_book:
+			data.append(':'.join([value for value in contact.values()]))
+		with open(self._path, 'w', encoding='UTF-8') as file:
+			file.write('\n'.join(data))
 
-def add_contact(contact: dict[str, str]):
-	global phone_book
-	phone_book.append(contact)
-	return contact.get('name')
+	def get_pb(self) -> list[dict[str, str]]:
+		return self._phone_book
 
-def del_contact(index: int): 
-	global phone_book
-	return phone_book.pop(index-1).get('name') 
+	def add_contact(self, contact: dict[str, str]):
+		self._phone_book.append(contact)
+		return contact.get('name')
 
-def search_pb(word: str) -> list[dict[str, str]]:
-	result: list[dict[str, str]] = []
-	for contact in phone_book:
-		for field in contact.values():
-			if word.lower().strip() in field.lower().strip():
-				result.append(contact)
-				break
-	return result
+	def del_contact(self, index: int): 
+		return self._phone_book.pop(index-1).get('name') 
 
-def change_pb(contact: dict, index: int):
-	global phone_book
-	
-	with suppress(Exception):
-		if len(contact['name']) > 0:
-			phone_book[index-1]['name'] = contact['name']
-	with suppress(Exception):
-		if len(contact['phone']) > 0:
-			phone_book[index-1]['phone'] = contact['phone']
-	with suppress(Exception):
-		if len(contact['comment']) > 0:
-			phone_book[index-1]['comment'] = contact['comment']
+	def search_pb(self, word: str) -> list[dict[str, str]]:
+		result: list[dict[str, str]] = []
+		for contact in self._phone_book:
+			for field in contact.values():
+				if word.lower().strip() in field.lower().strip():
+					result.append(contact)
+					break
+		return result
+
+	def change_pb(self, contact: dict, index: int):
+
+		with suppress(Exception):
+			if len(contact['name']) > 0:
+				self._phone_book[index-1]['name'] = contact['name']
+		with suppress(Exception):
+			if len(contact['phone']) > 0:
+				self._phone_book[index-1]['phone'] = contact['phone']
+		with suppress(Exception):
+			if len(contact['comment']) > 0:
+				self._phone_book[index-1]['comment'] = contact['comment']
